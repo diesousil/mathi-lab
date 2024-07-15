5451282150e19607f744637e406859314c764707
Diego Silva <contato@diegosilva.me>
2024-07-15 15:00:08 -0300

    Equation solver improved to consider angular coeficient when dealing with simple equations

--- src/input/Equation.js
+++ src/input/Equation.js
@@ -6,6 +6,17 @@ import MathExpression from "./MathExpression.js";
 class Equation extends InputMethod {
     constructor() {
         super();
+        this.mathExpressionSolver = null;
+    }
+
+    async solveMathExpression(expression) {
+        
+        if(this.mathExpressionSolver == null) {
+            this.mathExpressionSolver = new MathExpression();
+        }
+
+        return await this.mathExpressionSolver.process({ query: expression} );
+
     }
 
 
@@ -26,6 +37,33 @@ class Equation extends InputMethod {
         ];
     }
 
+    findPrevOperator(expression, number) {
+        const prevOperatorIndex = expression[1].findIndex(x => x[1] == number[1]-1);
+        let prevOperator = -1;
+
+        if(prevOperatorIndex > -1) {
+            prevOperator = expression[1][prevOperatorIndex];
+        }
+
+        return [prevOperator, prevOperatorIndex];
+
+    }
+
+    findPostOperator(expression, number) {
+        const expectedPostOperatorCharPos = number[1]+number[1].toString().length+1;
+        const postOperatorIndex = expression[1].findIndex(x => x[1] == expectedPostOperatorCharPos);
+        Logger.debug("expectedPostOperatorCharPos:" + expectedPostOperatorCharPos);
+        Logger.debug("found:" + postOperatorIndex);
+
+        let postOperator = -1;
+
+        if(postOperatorIndex > -1) {
+            postOperator = expression[1][postOperatorIndex];
+        }
+
+        return postOperator;
+    }
+
     moveNumbers(leftExpression, rightExpression, maxIndex) {
 
 
@@ -35,26 +73,81 @@ class Equation extends InputMethod {
 
         for(let i = 0;i<leftExpression[2].length;i++) {
             const number = leftExpression[2][i];
-            const operatorIndex = leftExpression[1].findIndex(x => x[1] == number[1]-1);
-            const operator = leftExpression[1][operatorIndex];
 
-            Logger.debug("dealing with number:" + shared.debugArray(number));
-            Logger.debug("its operator:" + shared.debugArray(operator));
+            Logger.debug("Number:" + number);
+            const [prevOperator, prevOperatorIndex] = this.findPrevOperator(leftExpression, number);
+            if(prevOperator != -1) {
 
-            let newOperator = shared.inverseOperator[operator[0]];
-            maxIndex += 1;
-            rightExpression[1].push([newOperator,maxIndex]);
-            maxIndex += 1;
-            number[1] = maxIndex;
-            rightExpression[2].push(number);
+                const postOperator = this.findPostOperator(leftExpression, number); 
 
-            leftExpression[2].splice(i,1);
-            leftExpression[1].splice(operatorIndex, 1)
+                if( ['*','/'].includes(postOperator)) {
+                    continue;
+                }
+
+                Logger.debug("dealing with number:" + shared.debugArray(number));
+                Logger.debug("its operator:" + shared.debugArray(prevOperator));
+    
+                let newOperator = shared.inverseOperator[prevOperator[0]];
+                maxIndex += 1;
+                rightExpression[1].push([newOperator,maxIndex]);
+                maxIndex += 1;
+                number[1] = maxIndex;
+                rightExpression[2].push(number);
+    
+                leftExpression[2].splice(i,1);
+                leftExpression[1].splice(prevOperatorIndex, 1)
+            }
         }
 
         Logger.debug("updated Left expression:" + shared.debugArray(leftExpression));
         Logger.debug("updated Right expression:" + shared.debugArray(rightExpression));
 
+    }
+
+    simplify(leftExpression, rightExpression, maxIndex) {
+
+
+        this.moveNumbers(leftExpression, rightExpression, maxIndex);
+
+    }
+
+    async solveSimplified(leftExpression, rightValue) {
+
+        let result = rightValue;
+
+        if(leftExpression[2].length > 0) {
+            let number = leftExpression[2][0][0].toString();
+            const operatorToReplace = leftExpression[1][0][0];
+            Logger.info("operatorToReplace:" + operatorToReplace);
+            const operator = shared.inverseOperator[operatorToReplace];
+            Logger.info("operator:" + operator);
+            let expression = result.toString() + operator + number;
+            Logger.info("New expression:" + expression);
+            result = await this.solveMathExpression(expression);
+        }
+
+        return result;
+    }
+
+    async solve(query) {
+
+        let maxIndex = query.length;
+
+        let [leftExpression, rightExpression] = query.split('=').map(x => this.extractCompents(x));
+
+        this.simplify(leftExpression, rightExpression, maxIndex);
+        let expression = this.rebuildExpression(rightExpression);
+
+        Logger.debug("rebuilt right expression before solve:" + expression);
+
+        let result = await this.solveMathExpression(expression);
+         
+        Logger.info("Partial result:" + result);
+        Logger.info("Left Expression:" + shared.debugArray(leftExpression));
+
+        result = await this.solveSimplified(leftExpression, result);
+
+        return result;
     }
 
     rebuildExpression(expressionArrays) {
@@ -75,26 +168,12 @@ class Equation extends InputMethod {
     async process(req) {
         Logger.info("== Started Equation process ==");
         Logger.debug("Original input: " + req.query);
-        let query = req.query;
-        let maxIndex = query.length;
 
-        /*
-        if(!this.validate(query)) {
-            return "error";
-        }
-            */
-        
-        let [leftExpression, rightExpression] = query.split('=').map(x => this.extractCompents(x));
+        let result = await this.solve(req.query);
 
-        this.moveNumbers(leftExpression, rightExpression, maxIndex);
-        const expression = this.rebuildExpression(rightExpression);
-        Logger.debug("rebuilt right expression before solve:" + expression);
+        Logger.debug("Final result: " + result);
 
-        let mathExpression = new MathExpression();
-        let finalResult = await mathExpression.process({ query: expression} );
-        Logger.info("Final result:" + finalResult);
-
-        return finalResult;
+        return result;
     }
 }
 
